d78cd6c5506435bfe561dff8983ac6f58f550523
Sean Rodman <srodman7689@gmail.com>
2022-07-27 15:00:42 -0500

    Updates

---
+++ component---src-pages-about-tsx-549297e43fbc31b075b4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadhd_at_33=self.webpackChunkadhd_at_33||[]).push([[49],{9205:function(e,t,a){a.r(t),a.d(t,{default:function(){return c}});var n=a(7294),r=a(9980),i=a(7059);function u(e){var t=e.src;return n.createElement(i.G,{image:(0,i.c)(t),alt:"This is is me, Sean Rodman",layout:"fixed",imgStyle:{verticalAlign:"middle",borderRadius:"50%"},width:50,height:50})}var c=(0,r.b)((function(){return n.createElement(n.Fragment,null,n.createElement(u,{src:"../images/about.jpg"}))}))}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-549297e43fbc31b075b4.js.map
